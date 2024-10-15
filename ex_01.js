f3313b5e100a5bdd906cba4cba9113b796ef0e04
QueenMae <maena.brice@epitech.digital>
2024-10-15 20:55:32 +0200

    ex_01.js

---
+++ ex_01.js
@@ -0,0 +1,7 @@
+function display_weather_chart(longitude, latitude) {
+    const img = document.createElement("img");
+    img.src = `https://www.7timer.info/bin/civil.php?lon=${longitude}&lat=${latitude}`;
+    img.alt = "weather forecast chart";
+    document.body.appendChild(img);
+}
+display_weather_chart(113.17, 23.09);
